3dddb4034098cf289c0e2708a1d1810f16761d7f
Leon <1778064763@qq.com>
2025-08-26 13:43:29 +0800

    update object

---
+++ src/object.cpp
@@ -0,0 +1,208 @@
+#include "object.hpp"
+#include <iostream>
+
+namespace object
+{
+
+    // Box 实现 (无变化)
+    Box::Box(float l, float t, float r, float b)
+        : left(l), top(t), right(r), bottom(b) {}
+
+    std::ostream &operator<<(std::ostream &os, const Box &box)
+    {
+        os << "{ \"left\": " << box.left
+           << ", \"top\": " << box.top
+           << ", \"right\": " << box.right
+           << ", \"bottom\": " << box.bottom
+           << " }";
+        return os;
+    }
+
+    // PosePoint 实现 (无变化)
+    PosePoint::PosePoint(float x, float y, float vis) : x(x), y(y), vis(vis) {}
+
+    std::ostream &operator<<(std::ostream &os, const PosePoint &point)
+    {
+        os << "{ \"x\": " << point.x
+           << ", \"y\": " << point.y
+           << ", \"vis\": " << point.vis
+           << " }";
+        return os;
+    }
+
+    // Obb 实现 (无变化)
+    Obb::Obb(float cx, float cy, float w, float h, float angle)
+        : cx(cx), cy(cy), w(w), h(h), angle(angle) {}
+
+    std::ostream &operator<<(std::ostream &os, const Obb &obb)
+    {
+        os << "{ \"cx\": " << obb.cx
+           << ", \"cy\": " << obb.cy
+           << ", \"w\": " << obb.w
+           << ", \"h\": " << obb.h
+           << ", \"angle\": " << obb.angle
+           << " }";
+        return os;
+    }
+
+    // Segmentation 实现 (无变化)
+    Segmentation::Segmentation(const Segmentation &other) : mask(other.mask.clone()) {}
+
+    Segmentation &Segmentation::operator=(const Segmentation &other)
+    {
+        if (this != &other)
+        {
+            mask = other.mask.clone();
+        }
+        return *this;
+    }
+
+    // Depth 实现 (无变化)
+    Depth::Depth(const Depth &other) : depth(other.depth.clone()), fog_data(other.fog_data) {}
+
+    Depth &Depth::operator=(const Depth &other)
+    {
+        if (this != &other)
+        {
+            depth = other.depth.clone();
+            fog_data = other.fog_data;
+        }
+        return *this;
+    }
+
+    float Depth::point_depth(int x, int y) const
+    {
+        if (x < 0 || y < 0 || x >= depth.cols || y >= depth.rows)
+            return 0.0f;
+        return depth.at<float>(y, x);
+    }
+
+    float Depth::average_depth() const
+    {
+        if (depth.empty())
+            return 0.0f;
+        cv::Scalar mean_val = cv::mean(depth);
+        return static_cast<float>(mean_val[0]);
+    }
+
+    float Depth::min_depth() const
+    {
+        if (depth.empty())
+            return 0.0f;
+        double min_val;
+        cv::minMaxLoc(depth, &min_val, nullptr);
+        return static_cast<float>(min_val);
+    }
+
+    float Depth::max_depth() const
+    {
+        if (depth.empty())
+            return 0.0f;
+        double max_val;
+        cv::minMaxLoc(depth, nullptr, &max_val, nullptr, nullptr);
+        return static_cast<float>(max_val);
+    }
+
+    float Depth::area_average_depth(const cv::Mat &seg) const
+    {
+        if (depth.empty() || seg.empty())
+            return 0.0f;
+
+        cv::Mat masked_depth;
+        depth.copyTo(masked_depth, seg);
+
+        float sum_depth = cv::sum(masked_depth)[0];
+        float area = cv::countNonZero(seg);
+        return area > 0 ? sum_depth / area : 0.0f;
+    }
+
+    float Depth::area_average_depth(const Box &box) const
+    {
+        if (depth.empty())
+            return 0.0f;
+
+        int left = std::max(0, static_cast<int>(box.left));
+        int top = std::max(0, static_cast<int>(box.top));
+        int right = std::min(depth.cols - 1, static_cast<int>(box.right));
+        int bottom = std::min(depth.rows - 1, static_cast<int>(box.bottom));
+
+        if (left >= right || top >= bottom)
+            return 0.0f;
+
+        cv::Rect roi(left, top, right - left, bottom - top);
+        cv::Mat depth_roi = depth(roi);
+
+        float sum_depth = cv::sum(depth_roi)[0];
+        float area = depth_roi.total();
+        return area > 0 ? sum_depth / area : 0.0f;
+    }
+
+    std::ostream &operator<<(std::ostream &os, const DetectionBox &box)
+    {
+        auto print_common_fields = [&](const DetectionBox &b)
+        {
+            os << ", \"score\": " << b.score
+               << ", \"class_id\": " << b.class_id
+               << ", \"class_name\": \"" << b.class_name << "\"";
+        };
+
+        switch (box.type)
+        {
+        case ObjectType::DETECTION:
+            os << "{ \"type\": \"DETECTION\", \"box\": " << box.box;
+            print_common_fields(box);
+            os << " }";
+            break;
+
+        case ObjectType::POSE:
+            os << "{ \"type\": \"POSE\", \"pose_points\": [";
+            for (size_t i = 0; i < box.pose_points.size(); ++i)
+            {
+                os << box.pose_points[i];
+                if (i < box.pose_points.size() - 1)
+                {
+                    os << ", ";
+                }
+            }
+            os << "]";
+            print_common_fields(box);
+            os << " }";
+            break;
+
+        case ObjectType::OBB:
+            os << "{ \"type\": \"OBB\", \"obb\": " << box.obb;
+            print_common_fields(box);
+            os << " }";
+            break;
+
+        case ObjectType::SEGMENTATION:
+            os << "{ \"type\": \"SEGMENTATION\", \"box\": " << box.box;
+            print_common_fields(box);
+            os << " }";
+            break;
+
+        case ObjectType::TRACK:
+            os << "{ \"type\": \"TRACK\", \"box\": " << box.box
+               << ", \"track_id\": " << box.track_id;
+            print_common_fields(box);
+            os << " }";
+            break;
+
+        case ObjectType::DEPTH_ANYTHING:
+            os << "{ \"type\": \"DEPTH_ANYTHING\" }";
+            break;
+
+        case ObjectType::DEPTH_PRO:
+            os << "{ \"type\": \"DEPTH_PRO\" }";
+            break;
+
+        case ObjectType::UNKNOW:
+        case ObjectType::POSITION: // 假设 POSITION 暂时没有特定输出
+        default:
+            os << "{ \"type\": \"UNKNOW\" }";
+            break;
+        }
+        return os;
+    }
+
+} // namespace object
--- src/object.hpp
+++ src/object.hpp
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <vector>
+#include <ostream> // 包含 ostream 用于友元函数声明
 #include "opencv2/opencv.hpp"
 
 namespace object
@@ -31,27 +32,17 @@ namespace object
         float bottom = 0.0f;
 
         Box() = default;
-        Box(float l, float t, float r, float b)
-            : left(l), top(t), right(r), bottom(b) {}
+        Box(float l, float t, float r, float b);
 
-        // 获取矩形的宽度和高度
-        float width() const { return right - left; }
-        float height() const { return bottom - top; }
-        float center_x() const { return (left + right) / 2; }
-        float center_y() const { return (top + bottom) / 2; }
-        // 获取矩形的面积
-        float area() const { return width() * height(); }
+        // 简单函数，适合内联
+        float width() const noexcept { return right - left; }
+        float height() const noexcept { return bottom - top; }
+        float center_x() const noexcept { return (left + right) / 2; }
+        float center_y() const noexcept { return (top + bottom) / 2; }
+        float area() const noexcept { return width() * height(); }
 
-        // 重载cout
-        friend std::ostream &operator<<(std::ostream &os, const Box &box)
-        {
-            os << "{ \"left\": " << box.left
-               << ", \"top\": " << box.top
-               << ", \"right\": " << box.right
-               << ", \"bottom\": " << box.bottom
-               << " }";
-            return os;
-        }
+        // 声明友元函数
+        friend std::ostream &operator<<(std::ostream &os, const Box &box);
     };
 
     struct PosePoint
@@ -61,16 +52,10 @@ namespace object
         float vis = 0.0f;
 
         PosePoint() = default;
-        PosePoint(float x, float y, float vis) : x(x), y(y), vis(vis) {}
+        PosePoint(float x, float y, float vis);
 
-        friend std::ostream &operator<<(std::ostream &os, const PosePoint &point)
-        {
-            os << "{ \"x\": " << point.x
-               << ", \"y\": " << point.y
-               << ", \"vis\": " << point.vis
-               << " }";
-            return os;
-        }
+        // 声明友元函数
+        friend std::ostream &operator<<(std::ostream &os, const PosePoint &point);
     };
 
     struct Obb
@@ -82,43 +67,22 @@ namespace object
         float angle = 0.0f;
 
         Obb() = default;
-        Obb(float cx, float cy, float w, float h, float angle)
-            : cx(cx), cy(cy), w(w), h(h), angle(angle) {}
+        Obb(float cx, float cy, float w, float h, float angle);
 
+        // 简单函数，适合内联
         float area() const { return w * h; }
 
-        friend std::ostream &operator<<(std::ostream &os, const Obb &obb)
-        {
-            os << "{ \"cx\": " << obb.cx
-               << ", \"cy\": " << obb.cy
-               << ", \"w\": " << obb.w
-               << ", \"h\": " << obb.h
-               << ", \"angle\": " << obb.angle
-               << " }";
-            return os;
-        }
+        // 声明友元函数
+        friend std::ostream &operator<<(std::ostream &os, const Obb &obb);
     };
 
     struct Segmentation
     {
         cv::Mat mask;
-        // 默认构造函数
+
         Segmentation() = default;
-
-        // 拷贝构造函数：强制深拷贝 cv::Mat
-        Segmentation(const Segmentation &other) : mask(other.mask.clone()) {}
-
-        // 拷贝赋值运算符：强制深拷贝 cv::Mat
-        Segmentation &operator=(const Segmentation &other)
-        {
-            if (this != &other)
-            {
-                mask = other.mask.clone();
-            }
-            return *this;
-        }
-
-        // C++11/14/17 最好也添加移动语义，或者使用 = default
+        Segmentation(const Segmentation &other);            // 声明拷贝构造
+        Segmentation &operator=(const Segmentation &other); // 声明拷贝赋值
         Segmentation(Segmentation &&other) = default;
         Segmentation &operator=(Segmentation &&other) = default;
     };
@@ -126,95 +90,29 @@ namespace object
     struct Depth
     {
         cv::Mat depth;
-        float fog_data;
-        // 默认构造函数
+        float fog_data = 0.0f;
+
         Depth() = default;
-
-        // 拷贝构造函数：强制深拷贝 cv::Mat
-        Depth(const Depth &other) : depth(other.depth.clone()), fog_data(other.fog_data) {}
-        float point_depth(int x, int y) const { return depth.at<float>(y, x); }
-        float average_depth() const
-        {
-            if (depth.empty())
-                return 0.0f;
-            cv::Scalar mean_val = cv::mean(depth);
-            return static_cast<float>(mean_val[0]);
-        }
-        float min_depth() const
-        {
-            if (depth.empty())
-                return 0.0f;
-            double min_val;
-            cv::minMaxLoc(depth, &min_val, nullptr);
-            return static_cast<float>(min_val);
-        }
-        float max_depth() const
-        {
-            if (depth.empty())
-                return 0.0f;
-            double max_val;
-            cv::minMaxLoc(depth, nullptr, &max_val, nullptr, nullptr);
-            return static_cast<float>(max_val);
-        }
-
-        float area_average_depth(const cv::Mat& seg) const
-        {
-            if (depth.empty() || seg.empty())
-                return 0.0f;
-
-            cv::Mat masked_depth;
-            depth.copyTo(masked_depth, seg);
-
-            float sum_depth = cv::sum(masked_depth)[0];
-            float area = cv::countNonZero(seg);
-            return area > 0 ? sum_depth / area : 0.0f;
-        }
-
-        float area_average_depth(const Box& box) const
-        {
-            if (depth.empty())
-                return 0.0f;
-
-            int left = std::max(0, static_cast<int>(box.left));
-            int top = std::max(0, static_cast<int>(box.top));
-            int right = std::min(depth.cols - 1, static_cast<int>(box.right));
-            int bottom = std::min(depth.rows - 1, static_cast<int>(box.bottom));
-
-            if (left >= right || top >= bottom)
-                return 0.0f;
-
-            cv::Rect roi(left, top, right - left, bottom - top);
-            cv::Mat depth_roi = depth(roi);
-
-            float sum_depth = cv::sum(depth_roi)[0];
-            float area = depth_roi.total();
-            return area > 0 ? sum_depth / area : 0.0f;
-        }
-
-        // 拷贝赋值运算符：强制深拷贝 cv::Mat
-        Depth &operator=(const Depth &other)
-        {
-            if (this != &other)
-            {
-                depth = other.depth.clone();
-                fog_data = other.fog_data;
-            }
-            return *this;
-        }
+        Depth(const Depth &other);            // 声明拷贝构造
+        Depth &operator=(const Depth &other); // 声明拷贝赋值
         Depth(Depth &&other) = default;
         Depth &operator=(Depth &&other) = default;
+
+        // 声明成员函数
+        float point_depth(int x, int y) const;
+        float average_depth() const;
+        float min_depth() const;
+        float max_depth() const;
+        float area_average_depth(const cv::Mat &seg) const;
+        float area_average_depth(const Box &box) const;
     };
 
     struct DetectionBox
     {
         ObjectType type = ObjectType::UNKNOW;
-        // Bounding box coordinates
         Box box;
-        // Pose points for keypoints detection
         std::vector<PosePoint> pose_points;
-        // Oriented bounding box for OBB detection
         Obb obb;
-        // Segmentation mask for segmentation detection
         Segmentation segmentation;
         Depth depth;
 
@@ -223,72 +121,12 @@ namespace object
         int track_id = -1;
         std::string class_name;
 
-        friend std::ostream &operator<<(std::ostream &os, const DetectionBox &box)
-        {
-            // 根据 type 输出 JSON 格式
-            if (box.type == ObjectType::DETECTION)
-            {
-                os << "{ \"type\": \"DETECTION\", "
-                   << "\"box\": " << box.box
-                   << ", \"score\": " << box.score
-                   << ", \"class_id\": " << box.class_id
-                   << ", \"class_name\": \"" << box.class_name << "\" }";
-            }
-            else if (box.type == ObjectType::POSE)
-            {
-                os << "{ \"type\": \"POSE\", "
-                   << "\"pose_points\": [";
-                for (size_t i = 0; i < box.pose_points.size(); ++i)
-                {
-                    os << box.pose_points[i];
-                    if (i < box.pose_points.size() - 1)
-                        os << ", ";
-                }
-                os << "], \"score\": " << box.score
-                   << ", \"class_id\": " << box.class_id
-                   << ", \"class_name\": \"" << box.class_name << "\" }";
-            }
-            else if (box.type == ObjectType::OBB)
-            {
-                os << "{ \"type\": \"OBB\", "
-                   << "\"obb\": " << box.obb
-                   << ", \"score\": " << box.score
-                   << ", \"class_id\": " << box.class_id
-                   << ", \"class_name\": \"" << box.class_name << "\" }";
-            }
-            else if (box.type == ObjectType::SEGMENTATION)
-            {
-                os << "{ \"type\": \"SEGMENTATION\", "
-                   << "\"box\": " << box.box
-                   << ", \"score\": " << box.score
-                   << ", \"class_id\": " << box.class_id
-                   << ", \"class_name\": \"" << box.class_name << "\" }";
-            }
-            else if (box.type == ObjectType::DEPTH_ANYTHING || box.type == ObjectType::DEPTH_PRO)
-            {
-                os << "{ \"type\": \""
-                   << (box.type == ObjectType::DEPTH_ANYTHING ? "DEPTH_ANYTHING" : "DEPTH_PRO")
-                   << "\" }";
-            }
-            else if (box.type == ObjectType::TRACK)
-            {
-                os << "{ \"type\": \"TRACK\", "
-                   << "\"box\": " << box.box
-                   << ", \"track_id\": " << box.track_id
-                   << ", \"score\": " << box.score
-                   << ", \"class_id\": " << box.class_id
-                   << ", \"class_name\": \"" << box.class_name << "\" }";
-            }
-            else
-            {
-                os << "{ \"type\": \"UNKNOW\" }";
-            }
-            return os;
-        }
+        // 声明友元函数
+        friend std::ostream &operator<<(std::ostream &os, const DetectionBox &box);
     };
 
     using DetectionBoxArray = std::vector<DetectionBox>;
 
-}
+} // namespace object
 
-#endif // endif OBJECT_HPP
+#endif // OBJECT_HPP
